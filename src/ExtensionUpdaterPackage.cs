20b1e0451cf775a0768eafc3b0269938bf646114
Mads Kristensen <post@madskristensen.net>
2015-01-07 16:45:27 -0800

    v1.3

--- src/ExtensionUpdaterPackage.cs
+++ src/ExtensionUpdaterPackage.cs
@@ -15,7 +15,7 @@ namespace MadsKristensen.ExtensionUpdater
     [Guid(GuidList.guidExtensionUpdaterPkgString)]
     public sealed class ExtensionUpdaterPackage : ExtensionPointPackage
     {
-        public const string Version = "1.2";
+        public const string Version = "1.3";
 
         protected override void Initialize()
         {
