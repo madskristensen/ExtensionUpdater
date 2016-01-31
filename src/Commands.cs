0f5339ba9217ce94db13c3cecce0513c6164978e
benmccallum <ben.mccallum@live.com.au>
2016-01-31 19:02:17 +1100

    Finished PR for import/export of extensions finally

--- src/Commands.cs
+++ src/Commands.cs
@@ -4,21 +4,31 @@ using System.ComponentModel.Design;
 using System.Linq;
 using Microsoft.VisualStudio.ExtensionManager;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio;
+using Microsoft.Win32;
+using System.IO;
+using System.Text;
 
 namespace MadsKristensen.ExtensionUpdater
 {
     class Commands
     {
         private static bool _hasLoaded = false;
+        private bool _isImportProcessing = false;
+        private string[] _toInstallExtensions = new string[] { };
+
         private IVsExtensionRepository _repository;
         private IVsExtensionManager _manager;
         private OleMenuCommandService _mcs;
+        private IVsOutputWindowPane _outputPane;
 
-        public Commands(IVsExtensionRepository repo, IVsExtensionManager manager, OleMenuCommandService mcs)
+        public Commands(IVsExtensionRepository repo, IVsExtensionManager manager, OleMenuCommandService mcs, IVsOutputWindowPane outputPane)
         {
             _repository = repo;
             _manager = manager;
             _mcs = mcs;
+            _outputPane = outputPane;
         }
 
         public void Initialize()
@@ -36,19 +46,210 @@ namespace MadsKristensen.ExtensionUpdater
             //OleMenuCommand search = new OleMenuCommand(Search, searchCommandID);
             //_mcs.AddCommand(search);
 
-            CommandID exportImportCommandID = new CommandID(GuidList.guidExtensionUpdaterCmdSet, (int)PkgCmdIDList.cmdExportImport);
-            OleMenuCommand exportImport = new OleMenuCommand(ExportImport, exportImportCommandID);
-            _mcs.AddCommand(exportImport);
+            CommandID importCommandID = new CommandID(GuidList.guidImportExportCmdSet, (int)PkgCmdIDList.cmdImport);
+            OleMenuCommand import = new OleMenuCommand(Import, importCommandID);
+            _mcs.AddCommand(import);
+
+            CommandID exportCommandID = new CommandID(GuidList.guidImportExportCmdSet, (int)PkgCmdIDList.cmdExport);
+            OleMenuCommand export = new OleMenuCommand(Export, exportCommandID);
+            _mcs.AddCommand(export);
         }
 
-        private void ExportImport(object sender, EventArgs e)
+        private void WriteToOutputPane(string message)
         {
-            Dialog.ExportImportDialog exportImportBox = new Dialog.ExportImportDialog();
-            exportImportBox._manager = _manager;
-            exportImportBox._repository = _repository;
-            exportImportBox.ShowActivated = true;
-            exportImportBox.ShowDialog();
+            _outputPane.OutputString(message + Environment.NewLine);
+            _outputPane.Activate();
+        }
+
+        #region Import / export
+        
+        private void Import(object sender, EventArgs e)
+        {
+            if (_isImportProcessing)
+            {
+                WriteToOutputPane("Extensions import ignored - one is currently already running.");
+                return;
+            }
+
+            var openFileDialog = new OpenFileDialog()
+            {
+                AddExtension = true,
+                DefaultExt = ".vsextensionslist",
+                CheckPathExists = true,
+                Filter = "Extensions List (.vsextensionslist)|*.vsextensionslist",
+                FilterIndex = 1,
+                //InitialDirectory = "%userprofile%",
+            };
+
+            var userClickedOK = openFileDialog.ShowDialog() ?? false;
+            if (!userClickedOK)
+            {
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(openFileDialog.FileName))
+            {
+                WriteToOutputPane("Extensions not imported - please select a file.");
+                return;
+            }
+
+            _isImportProcessing = true;
+            WriteToOutputPane("Importing extensions...");
+
+            string[] importFileLines = null;
+            try
+            {
+                importFileLines = File.ReadAllLines(openFileDialog.FileName).Where(l => !String.IsNullOrWhiteSpace(l)).Select(l => l.Trim()).ToArray();
+            }
+            catch
+            {
+                WriteToOutputPane("Error accessing/reading import file.");
+                _isImportProcessing = false;
+                return;
+            }
+
+            if (!importFileLines.Any())
+            {
+                WriteToOutputPane("No extensions were found in the import file.");
+                _isImportProcessing = false;
+                return;
+            }
+            
+            // Get extensions not already installed
+            var _installedExtensions = Commands.GetExtensions(_manager).ToDictionary(ie => ie.Header.Identifier, ie => ie.Header.Name);
+            _toInstallExtensions = importFileLines.Where(l => _installedExtensions.All(ie => ie.Key != l)).ToArray();
+
+            if (!_toInstallExtensions.Any())
+            {
+                WriteToOutputPane("You've already got all the extensions listed in the import file.");
+                _isImportProcessing = false;
+                return;
+            }
+
+            // Query for the complete new extension objects
+            var query = _repository.CreateQuery<GalleryEntry>(false, true, "ExtensionManagerQuery")
+                .Where(entry => _toInstallExtensions.Contains(entry.VsixID))
+                .OrderBy(entry => entry.Name)
+                .Skip(0)
+                .Take(500)
+                 as IVsExtensionRepositoryQuery<GalleryEntry>;
+
+            WriteToOutputPane(
+                string.Format("Looking up {0} potentially new extension/s in the gallery after skipping {1} already installed extension/s...", 
+                    _toInstallExtensions.Length, 
+                    importFileLines.Length - _toInstallExtensions.Length
+                )
+            );
+
+            query.ExecuteCompleted += Query_ExecuteCompleted;
+            query.ExecuteAsync();
 		}
+
+        private void Query_ExecuteCompleted(object sender, ExecuteCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                WriteToOutputPane("Error looking up new extension/s in the gallery...");
+                _isImportProcessing = false;
+                return;
+            }
+
+            // Extract results of found extensions
+            var foundExtensions =  e.Results.Cast<GalleryEntry>().ToArray();
+            var installableExtensions = foundExtensions.Where(entry => _toInstallExtensions.Any(l => l != entry.VsixID)).ToArray();
+            var missingExtensions = _toInstallExtensions.Except(foundExtensions.Select(fe => fe.VsixID)).ToArray();
+
+            if (!installableExtensions.Any())
+            {
+                WriteToOutputPane("Couldn't find any of the new extension/s in the gallery.");
+                _isImportProcessing = false;
+                return;
+            }
+
+            if (missingExtensions.Any())
+            {
+                WriteToOutputPane("Couldn't find " + missingExtensions.Length + " of the new extension/s in the gallery.");
+            }
+
+            // Download and install the new ones
+            WriteToOutputPane("Installing new extension/s:");
+            var wasAnInstallSuccessful = false;
+            foreach (var installableExtension in installableExtensions)
+            {
+                var msg = string.Format(" - '{0}' ", installableExtension.Name);
+
+                IInstallableExtension extension = null;
+                try
+                {
+                    extension = _repository.Download(installableExtension);
+                    _manager.Install(extension, false);
+                    msg += "installed.";
+                    wasAnInstallSuccessful = true;
+                }
+                catch (Exception ex)
+                {
+                    msg += "install failed. " + ex.Message;
+                }
+
+                WriteToOutputPane(msg);
+            }
+
+            if (wasAnInstallSuccessful)
+            {
+                WriteToOutputPane(Environment.NewLine + Environment.NewLine + "Please restart for changes to take affect.");
+            }
+
+            WriteToOutputPane("Extensions imported."); 
+
+            // Reset
+            _isImportProcessing = false;
+            _toInstallExtensions = new string[] { };
+        }
+                
+        private void Export(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog()
+            {
+                AddExtension = true,
+                DefaultExt = ".vsextensionslist",
+                CheckPathExists = true,
+                Filter = "Extensions List (.vsextensionslist)|*.vsextensionslist",
+                FilterIndex = 1,
+                //InitialDirectory = "%userprofile%",
+            };
+
+            var userClickedOK = saveFileDialog.ShowDialog() ?? false;
+            if (!userClickedOK)
+            {
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(saveFileDialog.FileName))
+            {
+                WriteToOutputPane("Extensions not exported - please choose a filename.");
+            }
+
+            WriteToOutputPane("Exporting your extensions...");
+
+            var installedExtensions = Commands.GetExtensions(_manager);
+            var sbInstalledExtensions = new StringBuilder(installedExtensions.Count() * 50);
+            foreach (var ext in installedExtensions)
+            {
+                sbInstalledExtensions.AppendLine(ext.Header.Identifier);
+            }
+
+            try
+            {
+                File.WriteAllText(saveFileDialog.FileName, sbInstalledExtensions.ToString());
+                WriteToOutputPane("Extensions exported.");
+            }
+            catch
+            {
+                WriteToOutputPane("Problem exporting extensions.");
+            }
+        }
+
+        #endregion
 
         //private void Search(object sender, EventArgs e)
         //{
--- src/Guids.cs
+++ src/Guids.cs
@@ -5,11 +5,18 @@ namespace MadsKristensen.ExtensionUpdater
     static class GuidList
     {
         public const string guidExtensionUpdaterPkgString = "cb8900a0-79fb-4c76-a6d6-f4e88d3384d2";
+
         public const string guidExtensionUpdaterCmdSetString = "c1703d5f-c150-4977-a4a7-d4a6094412bf";
         public const string guidFlyoutMenuString = "c1703d5f-c150-4977-a4a2-d4a6094412ba";
 
+        public const string guidImportExportCmdSetString = "c1703d5f-c150-4977-a4a7-d4a6094412be";
+        public const string guidFlyoutImportExportMenuString = "c1703d5f-c150-4977-a4a2-d4a6094412bb";
+        
         public static readonly Guid guidExtensionUpdaterCmdSet = new Guid(guidExtensionUpdaterCmdSetString);
         public static readonly Guid guidFlyoutMenu = new Guid(guidFlyoutMenuString);
+
+        public static readonly Guid guidImportExportCmdSet = new Guid(guidImportExportCmdSetString);
+        public static readonly Guid guidFlyoutImportExportMenu = new Guid(guidFlyoutImportExportMenuString);
     }
 
     static class PkgCmdIDList
@@ -17,6 +24,8 @@ namespace MadsKristensen.ExtensionUpdater
         public const uint cmdEnableAutoUpdate = 0x100;
         public const uint cmdCheckAll = 0x200;
         public const uint cmdSearch = 0x300;
-        public const uint cmdExportImport = 0x400;
+
+        public const uint cmdImport = 0x400;
+        public const uint cmdExport = 0x500;
     }
 }
